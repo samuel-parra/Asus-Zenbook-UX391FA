9dbc9a8907f4a0d8078c88ce834a8d1751c8b424
Samuel Parra <samuel.parra@gmail.com>
2020-10-06 12:44:43 -0300

    file orders

--- btmousefix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "fix bluetooth mouse"
-
-echo 0 > /sys/kernel/debug/bluetooth/hci0/conn_latency
-echo 6 > /sys/kernel/debug/bluetooth/hci0/conn_min_interval
-echo 7 > /sys/kernel/debug/bluetooth/hci0/conn_max_interval
